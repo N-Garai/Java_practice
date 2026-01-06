9faf55d59f3484a95081028e38e0545e7b2e5a9d
N-Garai <betatesterpro30@gmail.com>
2026-01-06 20:56:56 +0530

    Day5/Shapecalculator

--- src/Day5/AnimalSound.java
+++ src/Day5/AnimalSound.java
@@ -1,9 +1,9 @@
-package Day5;
+
 
 /* Create an abstract class Animal with an abstract method makeSound() and a concrete method sleep(). 
 Create two subclasses Dog and Cat that extend Animal and implement the makeSound() method.
  */
-
+package Day5;
 // Abstract class Animal
 abstract class Animal{
     public abstract void makeSound();
---
+++ src/Day5/BasicCalculator.java
@@ -0,0 +1,27 @@
+/* Create an interface Calculator with methods add(int a, int b) and subtract(int a, int b).
+ Implement a class BasicCalculator that implements this interface.
+ */
+
+package Day5;
+interface Calculator {
+    int add(int a, int b);
+    int subtract(int a, int b);
+}
+class BasicCalculatorImpl implements Calculator {
+    @Override
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    @Override
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
+public class BasicCalculator {
+    public static void main(String[] args) {
+        BasicCalculatorImpl calculator = new BasicCalculatorImpl();
+        System.out.println("Addition: " + calculator.add(10, 5));
+        System.out.println("Subtraction: " + calculator.subtract(10, 5));
+    }
+}
